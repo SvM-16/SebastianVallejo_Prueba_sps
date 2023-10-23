1ffdebc55242b36e81484665f8a187a9edc4334a
sebastian Vallejo <sebastianvallejo084@gmail.com>
2023-10-23 09:12:38 -0500

    avance de pages en usuario

--- stack_react/src/App.jsx
+++ stack_react/src/App.jsx
@@ -1,27 +1,34 @@
-import {BrowserRouter, useRoutes} from 'react-router-dom'
+import { BrowserRouter, useRoutes } from 'react-router-dom'
 import './App.css'
 
 import Navbar from './Components/Navbar/Navbar'
 import Notas from '../src/Pages/Notas/Notas.jsx'
 import CrearNota from '../src/Pages/Crear_nota/CrearNota.jsx'
 import CrearUsuario from '../src/Pages/Create_usuario/CreateUsuario.jsx'
+import { UserProvider } from './context/userContext.jsx'
+// import { NotasProvider } from './context/notasContext.jsx'
 
 function Router() {
   let router = useRoutes([
-    {path:'/', element:<Notas />},
-    {path:'/CrearNota', element:<CrearNota/>},
-    {path:'/CrearUsuario', element:<CrearUsuario/>}
+    { path: '/', element: <Notas /> },
+    { path: '/CrearNota', element: <CrearNota /> },
+    { path: '/CrearUsuario', element: <CrearUsuario /> }
   ])
   return router
 }
 
 function App() {
-  return(
-    <BrowserRouter>
-      <Navbar/>
-        <Router>
-        </Router>
-    </BrowserRouter>
+  return (
+  
+      // <NotasProvider>
+        <UserProvider>
+          <BrowserRouter>
+          <Navbar />
+          <Router>
+          </Router>
+          </BrowserRouter>
+        </UserProvider>
+    // </NotasProvider>
   )
 }
 
